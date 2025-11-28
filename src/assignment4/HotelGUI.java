b8a87c092b6bf33c750e8f1196dde6db5015e4c5
Othniel Powell <othniel@live.ca>
2025-11-28 16:39:58 -0500

    Update HotelGUI.java
    Finished the button.

--- src/assignment4/HotelGUI.java
+++ src/assignment4/HotelGUI.java
@@ -1,23 +1,15 @@
 package assignment4;
 
 import javafx.application.Application;
-
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser;
 
-import java.io.File;
 import java.io.PrintWriter;
-import java.util.List;
 import java.util.Collections;
 import java.util.Comparator;
-
 
 
 public class HotelGUI extends Application{
@@ -41,8 +33,19 @@ public class HotelGUI extends Application{
 
         sortStarsButton.setOnAction(e -> sortByStars());
         sortPriceButton.setOnAction(e -> sortByPrice());
-        
-        VBox layout = new VBox(10);
+
+		saveButton.setOnAction(e -> {
+         try (PrintWriter writer = new PrintWriter("Sorted.txt")) {
+        for (Hotel hotel : hotels) {
+            writer.println(hotel.toString());
+        }
+        } catch (Exception ex) {
+        ex.printStackTrace();
+        }
+                          });
+
+		
+		VBox layout = new VBox(10);
 		layout.getChildren().addAll(hotelListView, sortStarsButton, sortPriceButton, saveButton);
         
         Scene scene = new Scene(layout, 400, 400);
